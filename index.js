18258369270ac0e8c1a61dc18e8f92c81134073b
palabhi017 <palabhi017@gmail.com>
2022-09-29 23:09:52 +0530

    Day-3 work

---
+++ index.js
@@ -0,0 +1,30 @@
+document.querySelector("#icon1").addEventListener("click",card1)
+document.querySelector("#icon2").addEventListener("click",card2)
+document.querySelector("#icon3").addEventListener("click",card3)
+document.querySelector("#icon4").addEventListener("click",card4)
+document.querySelector("#icon5").addEventListener("click",card5)
+let A = document.querySelector("#bigcard2")
+function card2(){
+    A.scrollIntoView()
+    
+}
+let B = document.querySelector("#bigcard1")
+function card1(){
+    B.scrollIntoView()
+    
+}
+let C = document.querySelector("#bigcard3")
+function card3(){
+    C.scrollIntoView()
+    
+}
+let D = document.querySelector("#bigcard4")
+function card4(){
+    D.scrollIntoView()
+    
+}
+let E = document.querySelector("#bigcard5")
+function card5(){
+    E.scrollIntoView()
+    
+}
